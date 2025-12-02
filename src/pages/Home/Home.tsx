c23988fceae135b70760254ab8cfcaa245a940da
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-12-03 00:45:55 +0800

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
 import Hero from '../../sections/Hero/Hero';
 import Services from '../../sections/Services/Services';
+import FutureUpgrade from '../../sections/FutureUpgrade/FutureUpgrade';
+import ElevatorService from '../../sections/ElevatorService/ElevatorService';
+import StairliftService from '../../sections/StairliftService/StairliftService';
 import './Home.css';
 
 const Home = () => {
@@ -7,6 +10,9 @@ const Home = () => {
     <div className="page home-page">
       <Hero />
       <Services />
+      <FutureUpgrade />
+      <ElevatorService />
+      <StairliftService />
     </div>
   );
 };
